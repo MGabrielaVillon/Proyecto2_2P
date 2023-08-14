8e09a1eb92b4cfaca795c0bd0bdbdb21ff32387a
MGabrielaVillon <117649344+MGabrielaVillon@users.noreply.github.com>
2023-08-14 13:28:16 -0500

    Update LoginController.java

--- Proyecto2Intento/src/main/java/com/mycompany/proyecto2intento/LoginController.java
+++ Proyecto2Intento/src/main/java/com/mycompany/proyecto2intento/LoginController.java
@@ -1,10 +1,13 @@
 
+
 package com.mycompany.proyecto2intento;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -23,5 +26,28 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void ingresar(ActionEvent event) {
+        try{
+            
+            String correo = usr_correo.getText();
+            String contr = usr_contrasenia.getText();
+            Persona p1 = new Persona(correo, contr);
+            Alert a = new Alert(Alert.AlertType.INFORMATION,"Validado");
+            a.show();
+            cleanTextField();
+        }catch(NumberFormatException e){
+            Alert a = new Alert(Alert.AlertType.INFORMATION,"Usuario incorrecto");
+            a.show();
+        }
+        
+    }
+    private void cleanTextField(){
+        usr_correo.setText("");
+        usr_contrasenia.setText("");
+    }
+    
+    
     
 }
